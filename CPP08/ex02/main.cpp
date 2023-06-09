ad5570c3b250f19a3936fdbe8e25f85cc1db2adb
amechain <amechain@student.42heilbronn.de>
2023-06-09 09:57:17 +0200

    ex02-cpp8

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,12 +6,36 @@
 /*   By: amechain <amechain@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 14:23:23 by amechain          #+#    #+#             */
-/*   Updated: 2023/06/01 17:45:56 by amechain         ###   ########.fr       */
+/*   Updated: 2023/06/08 18:05:03 by amechain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "MutantStack.hpp"
+//#include "MutantStack.hpp"
+
+# include <iostream>
+# include <stack>
+# include <list>
 
 int main( void ) {
 
+	std::list<int> l;
+	std::list<int>::iterator it;
+
+	for (it = l.begin(); it != l.end(); it++)
+		std::cout << *it << std::endl;
+
+	std::stack<int> s; // = MutantStack<int> m;
+	std::stack<int>::iterator test; // = MutantStack<int>::iterator test2;
+
+
+	// for (test = s.begin(); test != s.end(); test++)
+	// 	std::cout << *test << std::endl;
+
 }
+
+// std::stack<int> s , means create an object.
+// It's a stack full of int. By default a deque, since we did not precise C.
+// I cannot call begin() and end() on this object. Unlike vector, list, deque...
+
+// std::stack<int>::iterator test , is like Form::NotFound.
+// iterator is a class. 
